2dd34d3c297ee904b5917962e9416e89313f6aa0
Rohan Singh <41806939+ROHAN-SINGH-SISODIYA@users.noreply.github.com>
2021-06-22 11:23:56 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Todo from './Todo'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <Todo/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
